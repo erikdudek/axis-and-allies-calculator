2735fca85e4a0dfcd112153c61aab2ae60399656
erikdudek <erikmdudek@gmail.com>
2024-07-02 17:58:48 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './App.scss';
+
+import Simulator from './components/Simulator.js';
+
+function App(props) {
+  return (
+    <div className="App">
+      <header>
+        <h3 className="title">Axis and Allies FFA v0.2.0 Battle Simulator</h3>
+      </header>
+      <Simulator />
+    </div>
+  );
+}
+
+export default App;
