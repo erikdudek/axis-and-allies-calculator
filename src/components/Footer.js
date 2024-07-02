064b0063c65fab0e6c5555c77ff19d7d61af4714
erikdudek <erikmdudek@gmail.com>
2024-07-01 21:22:36 -0400

    Add files via upload

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,12 @@ export default function Footer(props) {
   return (
     <footer>
       <p>
-        Based on the <a href="https://axisallies.com/rules/axis-allies-rules-1942-2nd-edition.pdf">
-        Second Edition</a> ruleset.
+        Based on the <a href="https://docs.google.com/document/d/1kT3cORO7thcsnxQetEajmcVn-p-W9KUesxJzx_ZVN9g/edit">
+        Free for All v0.2.0</a> ruleset.
       </p>
       <p>
         Created by <a href="https://sinclairtarget.com">Sinclair Target</a>.
+		Modified by <a href="https://erikdudek.com">Erik Dudek</a>
       </p>
       <a href="https://github.com/sinclairtarget/axis-and-allies-calculator">
         <img src="/images/github.svg" />
